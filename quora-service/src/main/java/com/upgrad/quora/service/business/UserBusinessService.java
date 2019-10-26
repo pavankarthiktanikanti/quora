0d391d9a5d4e7f1e517e57776746ec1120d4c146
pavankarthiktanikanti <pavankarthiktanikanti@gmail.com>
2019-10-26 10:42:23 +0530

    Introduce User Signout Feature #9
    - Create new Entity UserAuthEntity class for user_auth table.
    
    - Add mapping /user/signout to provision user signout.
    
    - Update LogoutAt time of the user if access token is valid.
    
    - Add SignOutRestrictedException handler to RestExceptionHandler.
    
    - Throw error message when access token is expired or not signed in.

--- quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
@@ -2,18 +2,23 @@ package com.upgrad.quora.service.business;
 
 import com.upgrad.quora.service.dao.UserDao;
 import com.upgrad.quora.service.entity.User;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.exception.SignOutRestrictedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+
 @Service
 public class UserBusinessService {
 
+    private static final Long EIGHT_HOURS_IN_MILLIS = 8 * 60 * 60 * 1000L;
     @Autowired
     private UserDao userDao;
-
     @Autowired
     private PasswordCryptographyProvider cryptographyProvider;
 
@@ -41,5 +46,46 @@ public class UserBusinessService {
             throw new SignUpRestrictedException("SGR-002", "This user has already been registered, try with any other emailId");
         }
         return userDao.createUser(user);
+    }
+
+    /**
+     * This method validates the user session by making use of the access token
+     * If it is expired or invalid, then throws back the exception asking the user to sign in
+     * If the user session is active, then pulls the UUID of the user̥
+     *
+     * @param authorization holds the access token for authenticating the user
+     * @return uuid of the user
+     * @throws SignOutRestrictedException if the access token is expired or user never signed in
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public String getUserUUID(String authorization) throws SignOutRestrictedException {
+        UserAuthEntity userAuthEntity = userDao.getUserAuthToken(authorization);
+        if (isUserSessionValid(userAuthEntity)) {
+            userAuthEntity.setLogoutAt(ZonedDateTime.now());
+            userDao.updateUserAuthEntity(userAuthEntity);
+            return userAuthEntity.getUuid();
+        }
+        throw new SignOutRestrictedException("SGR-001", "User is not Signed in");
+    }
+
+    /**
+     * This method checks if the user session is active based on the access token/logout at
+     * If the user has ̥already signed out, even though the token isn't expired still he/she didn't login
+     * The access token expiry should always be in future, only then it isn't expired
+     * If the token expiry falls before the current time, then user should be asked to login
+     *
+     * @param userAuthEntity The authentication Entity object holding the information about user login and access token
+     * @return true if all session conditions satisfy, false otherwise
+     */
+    public Boolean isUserSessionValid(UserAuthEntity userAuthEntity) {
+        if (userAuthEntity != null && userAuthEntity.getLogoutAt() == null
+                && userAuthEntity.getExpiresAt() != null) {
+            Long timeDifference = ChronoUnit.MILLIS.between(ZonedDateTime.now(), userAuthEntity.getExpiresAt());
+            // Negative timeDifference indicates an expired access token,
+            // difference should be with in the limit, token will be expired after 8 hours
+            return (timeDifference >= 0 && timeDifference <= EIGHT_HOURS_IN_MILLIS);
+        }
+        // Token expired or user already logged out or user never signed in before(may also be the case of invalid token)
+        return false;
     }
 }
