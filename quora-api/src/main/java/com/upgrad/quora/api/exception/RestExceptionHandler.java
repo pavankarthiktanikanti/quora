0050d7f0b99d3075ca5283979ea3901638004ae0
Khilan10 <mahajankhilan1@gmail.com>
2019-11-09 21:29:16 +0530

    Changing HttpStatus to UNAUTHORIZED in authenticationFailedException Method #3

--- quora-api/src/main/java/com/upgrad/quora/api/exception/RestExceptionHandler.java
+++ quora-api/src/main/java/com/upgrad/quora/api/exception/RestExceptionHandler.java
@@ -41,7 +41,7 @@ public class RestExceptionHandler {
     public ResponseEntity<ErrorResponse> authenticationFailedException(AuthenticationFailedException exe, WebRequest request) {
 
         return new ResponseEntity<ErrorResponse>(
-                new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()), HttpStatus.NOT_FOUND
+                new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()), HttpStatus.UNAUTHORIZED
         );
     }
 
