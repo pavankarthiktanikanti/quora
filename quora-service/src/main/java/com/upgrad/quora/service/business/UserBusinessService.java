9bad4e80694e7c7fd4ada681b7131bb12ad6cab2
Khilan10 <mahajankhilan1@gmail.com>
2019-11-09 18:52:22 +0530

    Introduce User Sign in Feature #3
    -  Add mapping /user/signin to provide sign in functionality
    
    -  Added code to generate Auth token and store it in database

--- quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
@@ -3,10 +3,7 @@ package com.upgrad.quora.service.business;
 import com.upgrad.quora.service.dao.UserDao;
 import com.upgrad.quora.service.entity.User;
 import com.upgrad.quora.service.entity.UserAuthEntity;
-import com.upgrad.quora.service.exception.AuthorizationFailedException;
-import com.upgrad.quora.service.exception.SignOutRestrictedException;
-import com.upgrad.quora.service.exception.SignUpRestrictedException;
-import com.upgrad.quora.service.exception.UserNotFoundException;
+import com.upgrad.quora.service.exception.*;
 import com.upgrad.quora.service.util.QuoraUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +11,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
+import java.util.Base64;
 
 @Service
 public class UserBusinessService {
@@ -49,6 +46,49 @@ public class UserBusinessService {
         }
         return userDao.createUser(user);
     }
+
+    /**
+     * This method takes the authorization string which is encoded username and password
+     * If the username and password doesnot matches than it throws Authentication failed exception
+     * If the username and password match than auth token is generated
+     *
+     * @param authorization holds the basic access token used for authentication
+     * @return userAuthTokenEntity that conatins acess token and user UUID
+     * @throws AuthenticationFailedException if the username doesnot exists or password doesnot match
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserAuthEntity signIn(String authorization) throws AuthenticationFailedException {
+        //this will be used to decode the request header authorization
+        byte[] decode = Base64.getDecoder().decode(authorization.split(QuoraUtil.BASIC_TOKEN)[1]);
+        String decodedText = new String(decode);
+        String[] decodedArray = decodedText.split(QuoraUtil.COLON);
+        String username = decodedArray[0];
+        String password = decodedArray[1];
+        User user = userDao.getUserByUserName(username);
+        if (user == null) {
+            throw new AuthenticationFailedException("ATH-001", "This username does not exist");
+        }
+
+        final String encryptedPassword = cryptographyProvider.encrypt(password, user.getSalt());
+        if (encryptedPassword.equals(user.getPassword())) {
+
+            JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(encryptedPassword);
+            UserAuthEntity userAuthTokenEntity = new UserAuthEntity();
+            userAuthTokenEntity.setUser(user);
+            final ZonedDateTime now = ZonedDateTime.now();
+            final ZonedDateTime expiresAt = now.plusHours(8);
+            userAuthTokenEntity.setAccessToken(jwtTokenProvider.generateToken(user.getUuid(), now, expiresAt));
+            userAuthTokenEntity.setLoginAt(now);
+            userAuthTokenEntity.setExpiresAt(expiresAt);
+            userAuthTokenEntity.setUuid(user.getUuid());
+            return userDao.createAuthToken(userAuthTokenEntity);
+
+        } else {
+            throw new AuthenticationFailedException("ATH-002", "Password failed");
+        }
+
+    }
+
 
     /**
      * This method validates the user session by making use of the access token
